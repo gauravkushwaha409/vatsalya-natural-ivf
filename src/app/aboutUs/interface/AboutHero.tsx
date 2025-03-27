6d03301a783892de4ae06da5f58830b69502380f
manishpun08 <manishpunmagar6@gmail.com>
2025-03-27 11:19:16 +0545

    feat: Add About Us page with Our Culture and why choose us content

---
+++ src/app/aboutUs/interface/AboutHero.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import hero from "@/assests/about/aboutHero.png";
+import CustomBreadcrumb from "@/components/CustomBreadcrumb";
+
+const AboutHero = () => {
+  return (
+    <div>
+      <div className="relative w-full h-[500px] overflow-hidden">
+        {/* Background Image with Overlay */}
+        <div className="absolute inset-0 z-0">
+          <Image
+            src={hero}
+            alt="Happy couple with newborn baby"
+            fill
+            className="object-cover brightness-[0.35]"
+            priority
+          />
+        </div>
+
+        {/* Content */}
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white px-4 text-center">
+          <CustomBreadcrumb
+            items={[
+              {
+                name: "Home",
+                link: "/",
+              },
+              {
+                name: "About Us",
+              },
+            ]}
+            className="absolute top-4"
+          />
+
+          {/* Heading */}
+          <h1 className="typography-h3 font-bold mb-4">About Us</h1>
+          {/* Subheading */}
+          <p className="typography-paragraph-large font-medium mb-10">
+            We are dedicated to turning dreams of parenthood into reality
+            through personalized care and innovative fertility treatments.
+          </p>
+          {/* CTA Button */}
+          <Link
+            href="/appointment"
+            className="typography-h5 font-semibold border-[0.4px] border-secondary-100 hover:bg-secondary py-4 px-11 rounded-full text-lg transition-colors duration-300 shadow-[0px 8px 18px 0px rgba(101,53,83,0.62)] bg-gradient-to-r from-[#A0385A] to-[#3A142C]"
+          >
+            Book an Appointment
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutHero;
---
+++ src/app/aboutUs/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutHero from "./interface/AboutHero";
+import OurStory from "./interface/OurStory";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutHero />
+      <OurStory />
+    </div>
+  );
+};
+
+export default AboutUs;
