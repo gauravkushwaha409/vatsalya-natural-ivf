c429474fedc01e70a321f69ede96ffb36427e3ce
manishpun08 <manishpunmagar6@gmail.com>
2025-03-27 17:07:56 +0545

    fix: Update background gradient colors in OpenPosition component

--- src/app/career/partials/OpenPosition.tsx
+++ src/app/career/partials/OpenPosition.tsx
@@ -39,10 +39,7 @@ const jobListings = [
 const OpenPosition = () => {
   return (
     <div className="">
-      <div
-        className="w-full py-16 md:py-24 bg-gradient-to-r from-[#FFD2CE] to-[#EBC0DB]
-      "
-      >
+      <div className="w-full py-16 md:py-24 bg-gradient-to-r from-[#EBC0DB] to-primary-100">
         <div className="container mx-auto px-4 md:px-6">
           <div className="flex flex-col items-center text-center space-y-4 ">
             <div className="flex items-center w-full justify-center gap-4 max-w-3xl">
