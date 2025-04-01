71512049e13527223954d9c89ed9cd8f8262f324
Saugat <asaugat04@gmail.com>
2025-04-01 15:56:48 +0545

    fix: Adjust positioning of elements in Herosection for improved layout

--- src/app/ourExperts/partials/Herosection.tsx
+++ src/app/ourExperts/partials/Herosection.tsx
@@ -16,7 +16,7 @@ const Herosection = () => {
         <div className="top-5 left-1/2 z-30 absolute -translate-x-1/2 transform">
           <Breadcrumb name="OurExperts" baseName="Home" />
         </div>
-        <div className="top-1/3 left-0 lg:left-1/2 z-10 absolute px-5 text-white text-center lg-translate-x-1/2 translate-x-0 transform">
+        <div className="top-1/3 left-0 lg:left-1/2 z-10 absolute px-5 text-white text-center translate-x-0 lg:-translate-x-1/2 transform">
           <h1 className="pb-5 font-bold typography-h3">Our Experts</h1>
           <p className="font-normal !text-text-50 typography-paragraph-regular">
             Our team of experts is dedicated to providing advanced fertility
