de610f51344a9d12233ae31c66c228ffb109415f
prasannajoshitech <prasannajoshitech@gmail.com>
2025-04-06 15:21:07 +0545

    feat: Add axios instance and getData function for API requests; update BASE_API_URL and add formatDate utility function

---
+++ src/api/axios.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosResponse } from "axios";
+import { BASE_API_URL } from "./endpoints";
+const axiosInstance = axios.create({
+  baseURL: BASE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getData = async <T = any>(
+  url: string,
+  params?: any
+): Promise<AxiosResponse<T>> => {
+  const data = await axiosInstance.get(url, {
+    params,
+    headers: {
+      "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
+      Pragma: "no-cache",
+      Expires: "0",
+    },
+  });
+  // if (!data.data) throw new Error(`Error: ${data.data.message}`);
+  return data.data;
+};
