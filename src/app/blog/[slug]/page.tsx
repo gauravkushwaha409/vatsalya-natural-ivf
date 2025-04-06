6e2e7c0872e4021ac51bea09f468f10474a90de0
prasannajoshitech <prasannajoshitech@gmail.com>
2025-04-06 17:57:17 +0545

    refactor: Update BlogDetail and Blog components to use Promise for params and improve error handling

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -7,31 +7,34 @@ import { getData } from "@/api/axios";
 import { endpoints } from "@/api/endpoints";
 import ErrorMessage from "@/components/ErrorMessage";
 
-interface BlogDetailProps {
-  params: {
-    slug: string;
-  };
+interface BlogDetailPageProps {
+  params: Promise<{ slug: string }>;
 }
-const BlogDetail = async ({ params }: BlogDetailProps) => {
+const BlogDetail = async ({ params }: BlogDetailPageProps) => {
   try {
-    const slug = await params.slug;
-    const data = await getData(endpoints.blog + `/${slug}`);
+    const slug = await params;
+    const data = await getData(endpoints.blog + `/${slug?.slug}`);
+
     return (
       <div className="relative">
         <BlogDetailHero data={data?.data?.blog} />
-        <div className="px-5 lg:px-[11.25rem] ">
+        <div className="px-5 lg:px-[11.25rem]">
           <div className="absolute top-[23.5rem] md:top-[33.5rem] lg:top-1/2 left-5 lg:left-20">
             <Stats />
           </div>
           <BlogDescription data={data?.data?.blog} />
-          <CommentForm slug={slug} />
+          <CommentForm slug={slug?.slug} />
           <SimilarBlogs data={data?.data?.relatedBlogs} />
         </div>
       </div>
     );
   } catch (error) {
-    console.log("Error fetching blog details:", error);
-    <ErrorMessage />;
+    console.error("Error fetching blog details:", error);
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <ErrorMessage />
+      </div>
+    );
   }
 };
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,11 +4,16 @@ import { getData } from "@/api/axios";
 import { endpoints } from "@/api/endpoints";
 
 interface BlogProps {
-  searchParams: { [key: string]: string | string[] | undefined };
+  params: Promise<{ slug: string }>;
+
+  searchParams?: Promise<{
+    page?: string | string[]; // can be string or array, since query params can be an array
+  }>;
 }
+
 const Blog = async ({ searchParams }: BlogProps) => {
   try {
-    const page = Number(searchParams.page) || 1;
+    const page = (await Number((await searchParams)?.page)) || 1; // Convert to number, default to 1 if not present
     const perPage = 12;
     const data = await getData(
       endpoints.blog + `?page=${page}&perPage=${perPage}`
