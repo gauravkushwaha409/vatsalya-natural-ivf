edb8bc28165b70b1cd7f2584d21d6eaf8af72d18
prasannajoshitech <prasannajoshitech@gmail.com>
2025-04-16 12:53:31 +0545

    fix: Remove duplicate import of usePathname in Header component

--- src/commons/Header.tsx
+++ src/commons/Header.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import Schedule from "./partials/Schedule";
 import MobileNav from "./partials/MobileNav";
 import { usePathname } from "next/navigation";
-import { usePathname } from "next/navigation";
 
 const Header = () => {
   const pathame = usePathname();
