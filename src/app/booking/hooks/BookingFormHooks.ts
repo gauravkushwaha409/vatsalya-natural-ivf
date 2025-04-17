e2ddce1fa5c5e6873db0612904d76b9fd2258421
manishpun08 <manishpunmagar6@gmail.com>
2025-04-17 17:16:21 +0545

    fix: Reset form after successful booking submission

--- src/app/booking/hooks/BookingFormHooks.ts
+++ src/app/booking/hooks/BookingFormHooks.ts
@@ -55,6 +55,7 @@ export const useBookingForm = () => {
 
         if (response?.data?.status === "success") {
           showSuccessMessage(response?.data?.message);
+          formik.resetForm();
         }
       } catch (error) {
         console.error("Failed to fetch slots:", error);
