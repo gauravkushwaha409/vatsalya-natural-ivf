9123d0c4a0724c8db1bf0bde18a8f809a9cf42e4
Saugat <asaugat04@gmail.com>
2025-04-18 15:18:29 +0545

    feat: Add AnimatingLines component for enhanced visual effects

---
+++ src/app/(home)/partials/AnimatingLines.tsx
@@ -0,0 +1,64 @@
+
+const AnimatingLines = () => {
+  return (
+    <div className="relative">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1443 131"
+        fill="none"
+        className="absolute inset-0"
+      >
+        <path
+          d="m1442 40.7309s-90.39 53.8868-218.5 79.0241c-296.766 58.231-579.037-146.7081-880-115.5001-156.901 16.2697-342.5 103.7451-342.5 103.7451"
+          stroke="url(#paint0_linear_1060_2856)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_1060_2856"
+            x1="1"
+            y1="65.6154"
+            x2="1442"
+            y2="65.6154"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#FFBDB6" />
+            <stop offset="1" stop-color="#D3A3C1" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1443 130"
+        fill="none"
+        className="absolute inset-0"
+      >
+        <path
+          d="m1442 34.4999s-93.39 58.3628-221.5 83.5001c-296.766 58.231-576.037-144.9531-877-113.7451-156.901 16.2696-342.5 106.5001-342.5 106.5001"
+          stroke="url(#paint0_linear_1060_2857)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_1060_2857"
+            x1="1"
+            y1="64.7633"
+            x2="1442"
+            y2="64.7633"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#FFBDB6" />
+            <stop offset="1" stop-color="#D3A3C1" />
+          </linearGradient>
+        </defs>
+      </svg>
+      {/* <Image
+                  className="absolute"
+                  src="/home/hero-bottom-lines.png"
+                  alt="Illustration of circle"
+                  layout="responsive"
+                  width={1000}
+                  height={1000}
+                /> */}
+    </div>
+  );
+}
+export default AnimatingLines
