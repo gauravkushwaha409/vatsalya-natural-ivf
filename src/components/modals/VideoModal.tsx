043f094e3323470c7bbbfa65c79989872852f87b
Saugat <asaugat04@gmail.com>
2025-04-21 17:15:18 +0545

    fix: Adjust z-index value in VideoModal for proper layering

--- src/components/modals/VideoModal.tsx
+++ src/components/modals/VideoModal.tsx
@@ -25,7 +25,7 @@ export default function VideoModal({
     <>
       {isOpen &&
         ReactDOM.createPortal(
-          <div className="z-[99999] fixed inset-0 flex justify-center items-center bg-black/40 w-full h-screen text-black padding">
+          <div className="z-50 fixed inset-0 flex justify-center items-center bg-black/40 w-full h-screen text-black padding">
             <button
               onClick={onClose}
               className="top-[30%] lg:top-5 right-5 absolute flex justify-center items-center p-2 border border-white rounded-full w-10 h-10 cursor-pointer"
