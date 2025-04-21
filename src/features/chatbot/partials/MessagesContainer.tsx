d24bebcbb4951ab3e0601ede83070f4986913c19
Saugat <asaugat04@gmail.com>
2025-04-21 17:58:53 +0545

    refactor: Update chat message handling and improve type definitions

--- src/features/chatbot/partials/MessagesContainer.tsx
+++ src/features/chatbot/partials/MessagesContainer.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect } from "react";
-import { ChatMessage } from "../hooks/useChat";
+import { useAppSelector } from "@/store/store";
+import React from "react";
+import { IChatMessage } from "../interfaces/dto/message.type";
 import Message from "./Message";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const MessagesContainer: React.FC<{ messages: any }> = ({ messages }) => {
-  const [newMessage, setNewMessage] = React.useState([]);
-  useEffect(() => {
-    setNewMessage(messages);
-  }, [messages]);
-  console.log("messages testing new", newMessage);
-
+const MessagesContainer: React.FC<{ messages: IChatMessage[] }> = ({
+  messages,
+}) => {
+  const myid = useAppSelector((state) => state.chat.user?.id);
   return (
     <div className="space-y-5 mb-4 px-1.5 pt-4 w-full h-full">
-      <Message text="Hello, how can I help you?" sender="bot" />
-      {/* <Message text="Hello, how can I help you?" sender="user" name="saugat" />
-      <Message text="Hello, how can I help you?" sender="bot" /> */}
+      {/* <Message text="Hello, how can I help you?" sender="bot" /> */}
 
-      {newMessage?.map((message: ChatMessage, index) => (
-        <Message key={index} text={message?.message} sender={"user"} />
+      {messages?.map((message: IChatMessage, index) => (
+        <Message
+          key={index}
+          text={message?.message}
+          sender={message.sender_id === myid ? "user" : "bot"}
+        />
       ))}
     </div>
   );
