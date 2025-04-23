92fd2f6fdf404c168221a815eb63ab1c02251575
Saugat <asaugat04@gmail.com>
2025-04-23 10:38:05 +0545

    refactor: Enhance HTML structure and styling for ServiceDescription component

--- src/app/services/[slug]/partials/ServiceDescription.tsx
+++ src/app/services/[slug]/partials/ServiceDescription.tsx
@@ -16,14 +16,20 @@ const ServiceDescription: React.FC<IServiceDescription> = ({ data }) => {
 
   return (
     <article className="flex flex-col gap-5 py-10 text-text-400 text-justify">
-      <p dangerouslySetInnerHTML={{ __html: data?.description }} />
+      <p
+        className="max-w-none prose"
+        dangerouslySetInnerHTML={{ __html: data?.description }}
+      />
       {data?.serviceDetailsListSection?.map(
         (section: IServiceDetailsServiceDetailsListSection, index: number) => (
           <div key={index} className="flex flex-col gap-2">
             <div>
               <h4 className="font-medium typography-h4">{section?.title}</h4>
-              <p dangerouslySetInnerHTML={{ __html: section?.description }} />
-              {section?.listItems?.map((item, index) => (
+              <p
+                className="max-w-none prose"
+                dangerouslySetInnerHTML={{ __html: section?.description }}
+              />
+              {/* {section?.listItems?.map((item, index) => (
                 <div key={index}>
                   <p>{item?.listItemTitle}</p>
                   <p
@@ -32,13 +38,13 @@ const ServiceDescription: React.FC<IServiceDescription> = ({ data }) => {
                     }}
                   />
                 </div>
-              ))}
+              ))} */}
             </div>
             {section?.listItems && (
               <div className="flex flex-col gap-2">
                 {section.listItems.map((item) => (
                   <div className="flex gap-2" key={item._id}>
-                    <span className="inline-flex justify-center items-center bg-secondary-500 p-1 rounded-full size-7">
+                    <span className="inline-flex justify-center items-center bg-secondary-500 p-1 rounded-full size-7 shrink-0">
                       <IoMdCheckmark className="size-full text-white" />
                     </span>
                     <div>
@@ -46,6 +52,7 @@ const ServiceDescription: React.FC<IServiceDescription> = ({ data }) => {
                         {item?.listItemTitle}
                       </p>
                       <div
+                        className="max-w-none prose"
                         dangerouslySetInnerHTML={{
                           __html: item?.listItemDescription,
                         }}
