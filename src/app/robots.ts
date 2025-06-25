f3d56ee0f4ba3cbf6867cef0b30808a22313b013
prasannajoshitech <prasannajoshitech@gmail.com>
2025-06-25 16:25:13 +0545

    fix(robots): update sitemap URL to production domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `https://staging.vatsalaya.com/sitemap.xml`,
+    sitemap: `https://vatsalaya.com/sitemap.xml`,
   };
 }
