bab83deb7e2497a8de5e30ffcf6a38cb16c7962c
sumit ghimire <sumitghimire018@gmail.com>
2025-11-03 13:42:59 +0545

    feat:changes

--- src/app/(home)/partials/cta-section.tsx
+++ src/app/(home)/partials/cta-section.tsx
@@ -1,15 +1,21 @@
-import {  Mail, Phone } from "lucide-react";
+import { Mail, Phone } from "lucide-react";
 import Link from "next/link";
 
 const CTA = () => {
   return (
     <div className="py-9 lg:py-[72px] bg-[#ffd2ce] ">
-      <div className="max-w-[1440px] px-4 lg:px-0 mx-auto flex flex-col  gap-8 lg:gap-0 lg:flex-row lg:items-center justify-between ">
-        <div className="w-full lg:w-1/2 ">
+      <div className="max-w-[1440px] px-4 lg:px-0 mx-auto flex flex-col  gap-8 lg:gap-20 lg:flex-row lg:items-center justify-between ">
+        <div className="w-full lg:w-[100%] ">
           <p className="text-gray-800 text-2xl lg:text-[36px] font-semibold mt-2 ">
-           Connect With Us
+            Connect With Us
           </p>
-          <div className="flex items-start mt-4 lg:mt-8">
+          <p className="my-2">
+            {" "}
+            Page layouts look better with something in each section. Web page
+            designers, content writers, and layout artists use lorem ipsum
+          </p>
+
+          <div className="mt-8">
             <Link href={"/contact-us"}>
               <button
                 style={{
@@ -23,7 +29,27 @@ const CTA = () => {
             </Link>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 flex flex-col lg:flex-row gap-5 lg:gap-10">
+        {/* <div className="w-full lg:w-1/2 flex flex-col lg:flex-row gap-5 lg:gap-4">
+          <Link href={"/contact-us"}>
+            <button
+              style={{
+                boxShadow: "0px 5.486px 12.343px 0px rgba(215, 101, 120, 0.33)",
+              }}
+              className="flex items-center gap-3 bg-secondary-500 px-8 py-4 border border-secondary-200 rounded-full font-manrope font-extrabold text-white cursor-pointer typography-paragraph-regular"
+            >
+              Contact Us
+            </button>
+          </Link>
+          <Link href={"/contact-us"}>
+            <button
+              style={{
+                boxShadow: "0px 5.486px 12.343px 0px rgba(215, 101, 120, 0.33)",
+              }}
+              className="flex items-center gap-3 bg-secondary-500 px-8 py-4 border border-secondary-200 rounded-full font-manrope font-extrabold text-white cursor-pointer typography-paragraph-regular"
+            >
+              Contact Us
+            </button>
+          </Link>
           <div className="bg-white py-6 px-8 rounded-3xl w-full lg:w-auto flex-1 flex flex-col items-start gap-4">
             <div className="h-[52px] w-[52px] bg-[#a03879] rounded-full flex items-center justify-center">
               <Phone className="text-white" />
@@ -47,7 +73,7 @@ const CTA = () => {
               </p>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
