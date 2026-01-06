169a6e594a6380cf39793c199a25f21a7aa6e4e9
NirojBhattarai <neerajbhttri@gmail.com>
2026-01-06 17:02:59 +0545

    feat: add animated butterfly component and implement on home hero section

--- src/app/(home)/partials/HomeHero.tsx
+++ src/app/(home)/partials/HomeHero.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import RequestAppoimentModal from "@/components/modals/RequestAppoimentModal";
-import Image from "next/image";
 import { useState } from "react";
 import { IHomeData } from "../interface/home.interface";
+import ButterflyAnimation from "./ButterflyAnimation";
 
 type HomeProps = {
   data?: IHomeData;
@@ -16,10 +16,9 @@ const HomeHero: React.FC<HomeProps> = ({ data }) => {
   };
   return (
     <>
-      <div className=" relative flex flex-col lg:flex-row items-center min-h-[500px] md:min-h-[600px] lg:h-[660px] overflow-hidden">
+      <div className="relative flex lg:flex-row flex-col items-center lg:h-[660px] min-h-[500px] md:min-h-[600px] overflow-hidden">
         {/* Top Overlay Gradient */}
         <GradientOverlay position="top" />
-
         {/* Text Section */}
         <TextContent
           description={
@@ -29,18 +28,19 @@ const HomeHero: React.FC<HomeProps> = ({ data }) => {
           title={data?.subtitle ?? "We transform hope within you into life"}
           handleAppointmentClick={handleAppointmentClick}
         />
-
         {/* Video Section with Gradient Mask */}
         <BackgroundVideoSection />
         <VideoBlendGradient />
-
-        <Butterfly />
 
         {/* Background Gradient Layer */}
         <GradientLayer />
 
         {/* Bottom Overlay Gradient */}
         <GradientOverlay position="bottom" />
+
+        <div className="z-30 relative pointer-events-none">
+          <ButterflyAnimation />
+        </div>
       </div>
 
       <RequestAppoimentModal
@@ -73,14 +73,14 @@ const TextContent = ({
   description: string;
 }) => {
   return (
-    <div className="u-padding-l lg:w-[45%] shrink-0 z-30 relative pt-8 md:pt-12 lg:pt-0 px-4  order-2 lg:order-none">
-      <h1 className="text-2xl md:text-4xl lg:text-[52px] leading-tight lg:leading-[114.999%] font-extrabold mb-4 md:mb-6 lg:mb-4 text-[#A0377B] max-w-xl w-full">
+    <div className="z-30 relative order-2 lg:order-none px-4 pt-8 md:pt-12 lg:pt-0 lg:w-[45%] u-padding-l shrink-0">
+      <h1 className="mb-4 w-full max-w-xl font-extrabold text-[#A0377B] lg:text-[44px] xl:text-[52px] text-2xl md:text-3xl leading-snug md:leading-tight lg:leading-[114.999%] /">
         {title}
       </h1>
 
       {description && (
         <div
-          className="text-[#787878] text-sm md:text-base lg:text-[15px] leading-relaxed lg:leading-[180%] font-medium  max-w-xl w-full"
+          className="w-full max-w-xl font-medium text-[#787878] lg:text-[15px] text-sm md:text-base leading-relaxed lg:leading-[180%]"
           dangerouslySetInnerHTML={{
             __html: description,
           }}
@@ -101,7 +101,7 @@ const BookAppointment = ({
   return (
     <button
       onClick={handleAppointmentClick}
-      className="bg-gradient-to-r from-[#A0385A] to-[#3A142C] shadow-[0px_8px_18px_0px_rgba(101,53,83,0.62)] px-6 md:px-8 py-3 md:py-4 border-[0.4px] border-secondary-100 rounded-full font-semibold transition-all duration-300 text-white cursor-pointer typography-paragraph-regular hover:shadow-[0px_12px_24px_0px_rgba(101,53,83,0.8)] mt-6 md:mt-8 mb-12 lg:mb-0"
+      className="bg-gradient-to-r from-[#A0385A] to-[#3A142C] shadow-[0px_8px_18px_0px_rgba(101,53,83,0.62)] hover:shadow-[0px_12px_24px_0px_rgba(101,53,83,0.8)] mt-6 md:mt-8 mb-12 lg:mb-0 px-6 md:px-8 py-3 md:py-4 border-[0.4px] border-secondary-100 rounded-full font-semibold text-white transition-all duration-300 cursor-pointer typography-paragraph-regular"
     >
       Book Your Appointment
     </button>
@@ -110,14 +110,14 @@ const BookAppointment = ({
 
 const BackgroundVideoSection = () => {
   return (
-    <div className="absolute inset-y-0 lg:translate-x-[25%] lg:inset-0 w-full z-10 lg:z-0 order-1 lg:order-none">
+    <div className="z-10 lg:z-0 absolute inset-y-0 lg:inset-0 order-1 lg:order-none w-full lg:translate-x-[25%]">
       <video
         src="/home/video.mp4"
         autoPlay
         muted
         loop
         controls={false}
-        className="object-cover w-full h-full"
+        className="w-full h-full object-cover"
       />
     </div>
   );
@@ -125,27 +125,13 @@ const BackgroundVideoSection = () => {
 
 const GradientLayer = () => {
   return (
-    <div className="absolute z-20 h-340 left-0 w-1/2 bg-background-100 blur-[100px] border border-red-600" />
+    <div className="left-0 z-20 absolute bg-background-100 blur-[100px] border border-red-600 w-1/2 h-340" />
   );
 };
 
 const VideoBlendGradient = () => {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 lg:left-0 lg:inset-y-0 w-full lg:w-auto bg-gradient-to-t md:bg-gradient-to-r from-[#FFEDEC]/50 md:from-[#FFEDEC] via-[#FFE8E7]/30 md:via-[#FFE8E7]/40 to-transparent lg:to-transparent z-15 lg:z-20" />
-  );
-};
-
-const Butterfly = () => {
-  return (
-    <div className="hidden lg:block w-24 h-24 md:w-32 md:h-32 lg:w-[245px] lg:h-[243px] absolute -top-4 -left-4 md:-top-6 md:-left-8 lg:-top-10 lg:-left-26 z-21">
-      <Image
-        src="/svg/butterfly.svg"
-        alt="Butterfly"
-        width={100}
-        height={100}
-        className="object-cover w-full h-full"
-      />
-    </div>
+    <div className="top-0 right-0 bottom-0 left-0 lg:left-0 z-15 lg:z-20 absolute lg:inset-y-0 bg-gradient-to-t md:bg-gradient-to-r from-[#FFEDEC]/50 md:from-[#FFEDEC] via-[#FFE8E7]/30 md:via-[#FFE8E7]/40 to-transparent lg:to-transparent w-full lg:w-auto" />
   );
 };
 
